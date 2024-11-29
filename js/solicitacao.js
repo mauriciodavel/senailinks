528df67c098d4e0e5cffdcb5a3f0abaaf5c23ab1
Eduardo-DevJs <dudev2001@gmail.com>
2024-11-28 23:49:43 -0300

    Pagina de Solicitacao

--- js/solicitacao.js
+++ js/solicitacao.js
@@ -1,3 +1,29 @@
+const form = document.querySelector('#myForm');
+const select = document.querySelector("#citySelect");
+
+function changeFormAction(e) {
+  e.preventDefault();  // Previne o envio imediato do formulário (usado para teste)
+
+  // Altera o action baseado na escolha do usuário no select
+  switch(select.value) {
+    case 'vitoria':
+      form.action = 'https://formsubmit.co/mdavel@findes.org.br'; // Exemplo 
+      break;
+      case 'vila_velha':
+        form.action = 'https://formsubmit.co/ddluz@senai-es.org.br'; // Exemplo 
+        break;
+    case 'serra':
+      form.action = 'https://formsubmit.co/mluchini@findes.org.br'; 
+      break;
+    default:
+      form.action = 'https://formsubmit.co/mdavel@findes.org.br';
+      break;
+  }
+}
+
+select.addEventListener('change', changeFormAction);
+window.addEventListener('load', changeFormAction);
+
 const btnCancelar = document.getElementById("cancelar");
 btnCancelar.addEventListener("click", () => {
   window.location.replace("../pages/unidadeVitoria.html")
